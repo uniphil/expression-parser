ceca3045f992aa346fa1dd1f0af9ebf85cfe4b6e
Phil Schleihauf <uniphil@gmail.com>
2014-10-15 01:50:58 -0400

    accept that @mystor was right -- regex is already a state machine

--- lex.js
+++ lex.js
@@ -1,107 +1,30 @@
 var R = require('ramda');
 
-
-var tokenStates = [
-  {
-    name: 'space',
-    match: /\s/,
-    next: ['space'],
-    enter: true,
-    exit: 'space'
-  },
-  {
-    name: 'num',
-    match: /\d/,
-    next: ['num', 'dot'],
-    enter: true,
-    exit: 'literal'
-  },
-  {
-    name: 'dot',
-    match: /\./,
-    next: ['num_after_dot'],
-    enter: true,
-    exit: null
-  },
-  {
-    name: 'num_after_dot',
-    match: /\d/,
-    next: ['num_after_dot'],
-    enter: false,
-    exit: 'literal'
-  },
-  {
-    name: 'first_letter',
-    match: /[a-zA-Z_]/,  // like \w, but without 0-9
-    next: ['nth_letter'],
-    enter: true,
-    exit: 'name'
-  },
-  {
-    name: 'nth_letter',
-    match: /\w/,
-    next: ['nth_letter'],
-    enter: false,
-    exit: 'name'
-  },
-  {
-    name: 'paren',
-    match: /[\(\)\[\]]/,
-    next: [],
-    enter: true,
-    exit: 'paren'
-  },
-  {
-    name: 'caret',
-    match: /[\+\-\*\/\^<>]/,
-    next: [],
-    enter: true,
-    exit: 'operator'
-  },
+var tokenMatches = [
+  { type: 'space',    match: /^\s+/ },
+  { type: 'literal',  match: /^(\d*\.\d+|\d+)/ },
+  { type: 'name',     match: /^[a-zA-Z_]\w*/ },
+  { type: 'paren',    match: /^[\(\)\[\]]/ },
+  { type: 'operator', match: /^[\+\-\*\/\^<>]/ }
 ];
 
-
-// [tokenState] -> [names] -> tokenState
-var charToken = R.curry(function(tokenStates, allowedStates, chr) {
-  return R.find(R.where(R.mixin(
-    { match: function(tsRe) { return tsRe.test(chr); } },
-    allowedStates
-      ? { name: R.flip(R.contains)(allowedStates) }
-      : { enter: true }
-  )), tokenStates)
-});
-
-
-// str -> (token, str|undefined)
 var chomp = function(str) {
-  var i = 0,
-      token,
-      next = null,
-      exit = null;
-  while (1) {
-    token = charToken(tokenStates, next, str[i]);
-    if (!token || (i >= str.length)) break;
-    next = token.next;
-    exit = token.exit;
-    i++;
-  }
-
-  return {
-    token: { type: exit, value: R.substringTo(i, str) },
-    remainingStr: R.substringFrom(i, str)
-  };
+  var matcher = R.find(R.where({ match: function(re) { return re.test(str); } }), tokenMatches);
+  var token = { type: matcher ? matcher.type : null,
+                value: matcher ? str.match(matcher.match)[0] : str[0] };
+  return { token: token,
+           leftover: str.slice(token.value.length) };
 };
 
 
-// (str -> (a, str|undefined)) -> str -> [a]
-var chompReduce = R.curry(function(chomper, str) {
-  function reducer(initial, remainingStr) {
-    if (!remainingStr) return initial;  // nothing more to eat!
-    var result = chomper(remainingStr);
-    return reducer(R.append(result.token, initial), result.remainingStr);
+var chompReduce = function(str) {
+  function reducer(initial, leftover) {
+    if (!leftover) return initial;  // nothing more to eat!
+    var result = chomp(leftover);
+    return reducer(R.append(result.token, initial), result.leftover);
   }
   return reducer([], str);
-});
+};
 
 
-module.exports = chompReduce(chomp);
+module.exports = chompReduce;
